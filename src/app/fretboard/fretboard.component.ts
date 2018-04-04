77d50fe0358426adc5abe4ab2ab0de65d68c0392
Michal Vala <michal.vala@gmail.com>
2018-04-04 08:03:51 +0200

    fretboard component

---
+++ src/app/fretboard/fretboard.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Position } from '../position.model';
+
+@Component({
+  selector: 'app-fretboard',
+  templateUrl: './fretboard.component.html',
+  styleUrls: ['./fretboard.component.scss']
+})
+export class FretboardComponent implements OnInit {
+  loopNumbers = [];
+
+  @Input() position: Position;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.loopNumbers = Array(this.position.frets).map((x, i) => i);
+    this.loopNumbers.forEach(x => console.log(x));
+  }
+
+}
