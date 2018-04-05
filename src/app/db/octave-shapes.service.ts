3eb411b1f7dcf52925143e9a28ec8db1c1139d60
Michal Vala <michal.vala@gmail.com>
2018-04-05 07:39:46 +0200

    refactor octave shapes data init

--- src/app/db/octave-shapes.service.ts
+++ src/app/db/octave-shapes.service.ts
@@ -4,8 +4,8 @@ import { Shape } from '../model/shape';
 
 @Injectable()
 export class OctaveShapesService {
+  private readonly frets = 5;
   private data = {};
-
   private fingers = [];
 
   constructor() {
@@ -23,21 +23,21 @@ export class OctaveShapesService {
 
   private initData() {
     this.data = {
-      'C_1': { frets: 6, base: 0, fingers: this.fingers[1] },
-      'C_2': { frets: 6, base: 2, fingers: this.fingers[2] },
-      'C_3': { frets: 6, base: 4, fingers: this.fingers[3] },
-      'C_4': { frets: 6, base: 7, fingers: this.fingers[4] },
-      'C_5': { frets: 6, base: 9, fingers: this.fingers[5] },
-      'Csharp_1': { frets: 6, base: 1, fingers: this.fingers[1] },
-      'Csharp_2': { frets: 6, base: 3, fingers: this.fingers[2] },
-      'Csharp_3': { frets: 6, base: 5, fingers: this.fingers[3] },
-      'Csharp_4': { frets: 6, base: 8, fingers: this.fingers[4] },
-      'Csharp_5': { frets: 6, base: 10, fingers: this.fingers[5] },
-      'D_1': { frets: 6, base: 2, fingers: this.fingers[1] },
-      'D_2': { frets: 6, base: 4, fingers: this.fingers[2] },
-      'D_3': { frets: 6, base: 5, fingers: this.fingers[3] },
-      'D_4': { frets: 6, base: 9, fingers: this.fingers[4] },
-      'D_5': { frets: 6, base: 11, fingers: this.fingers[5] },
+      'C_1': { frets: this.frets, base: 0, fingers: this.fingers[1] },
+      'C_2': { frets: this.frets, base: 2, fingers: this.fingers[2] },
+      'C_3': { frets: this.frets, base: 4, fingers: this.fingers[3] },
+      'C_4': { frets: this.frets, base: 7, fingers: this.fingers[4] },
+      'C_5': { frets: this.frets, base: 9, fingers: this.fingers[5] },
+      'Csharp_1': { frets: this.frets, base: 1, fingers: this.fingers[1] },
+      'Csharp_2': { frets: this.frets, base: 3, fingers: this.fingers[2] },
+      'Csharp_3': { frets: this.frets, base: 5, fingers: this.fingers[3] },
+      'Csharp_4': { frets: this.frets, base: 8, fingers: this.fingers[4] },
+      'Csharp_5': { frets: this.frets, base: 10, fingers: this.fingers[5] },
+      'D_1': { frets: this.frets, base: 2, fingers: this.fingers[1] },
+      'D_2': { frets: this.frets, base: 4, fingers: this.fingers[2] },
+      'D_3': { frets: this.frets, base: 5, fingers: this.fingers[3] },
+      'D_4': { frets: this.frets, base: 9, fingers: this.fingers[4] },
+      'D_5': { frets: this.frets, base: 11, fingers: this.fingers[5] },
     };
   }
 
