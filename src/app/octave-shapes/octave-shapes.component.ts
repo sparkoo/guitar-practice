2461b65a802537e9a47a6945db02a46d281d31ae
Michal Vala <michal.vala@gmail.com>
2018-04-13 07:14:26 +0200

    draw tone list

--- src/app/octave-shapes/octave-shapes.component.ts
+++ src/app/octave-shapes/octave-shapes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { OctaveShapesService } from '../db/octave-shapes.service';
+import { Shape } from '../model/shape';
+import { ToneUp, ToneUpKeys, ToneValue } from '../model/tone';
 
 @Component({
   selector: 'app-octave-shapes',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./octave-shapes.component.scss']
 })
 export class OctaveShapesComponent implements OnInit {
+  shape: Shape = this.octaveShapesService.get('C_3');
 
-  constructor() { }
+  selectedTone: ToneValue = ToneUp['C'];
+
+  readonly tones = ToneUpKeys;
+  readonly ToneUp = ToneUp;
+
+  constructor(private octaveShapesService: OctaveShapesService) { }
 
   ngOnInit() {
   }
 
+  selectTone(tone: ToneValue) {
+    console.log(ToneUp[tone]);
+    this.selectedTone = tone;
+    this.drawShape();
+  }
+
+  drawShape() {
+    this.shape = this.octaveShapesService.get(`${this.selectedTone.key}_3`);
+  }
 }
