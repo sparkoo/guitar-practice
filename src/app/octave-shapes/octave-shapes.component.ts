3aea820e85cbc022fec90c6db5718453829e9bb9
Michal Vala <michal.vala@gmail.com>
2018-04-13 07:21:13 +0200

    cleanup

--- src/app/octave-shapes/octave-shapes.component.ts
+++ src/app/octave-shapes/octave-shapes.component.ts
@@ -25,7 +25,6 @@ export class OctaveShapesComponent implements OnInit {
   }
 
   selectTone(tone: ToneValue) {
-    console.log(ToneUp[tone]);
     this.selectedTone = tone;
     this.drawShape();
   }
