cdd68e7419290c4d0f11041cb3c4c1b608becc52
Michal Vala <michal.vala@gmail.com>
2018-04-13 21:57:04 +0200

    base shape component

---
+++ src/app/shape-base.component.ts
@@ -0,0 +1,53 @@
+import { ShapeNo, Tonality, TonalityValue, ToneUp, ToneUpKeys, ToneValue } from './model/tone';
+import { OnInit } from '@angular/core';
+import { Shape } from './model/shape';
+
+export abstract class ShapeBaseComponent implements OnInit {
+  shape: Shape;
+
+  selectedTone: ToneValue;
+  selectedShapeNo: number;
+  selectedTonality: TonalityValue;
+
+  readonly tones = ToneUpKeys;
+  readonly ToneUp = ToneUp;
+  readonly ShapeNo = ShapeNo;
+  readonly Tonality = Tonality;
+
+  protected constructor() {
+    this.selectedTone = this.initTone();
+    this.selectedShapeNo = this.initShapeNo();
+    this.selectedTonality = this.initTonality();
+  }
+
+  ngOnInit() {
+    this.drawShape();
+  }
+
+  selectTone(tone: ToneValue) {
+    this.selectedTone = tone;
+    this.drawShape();
+  }
+
+  selectShapeNo(shapeNo: number) {
+    this.selectedShapeNo = shapeNo;
+    this.drawShape();
+  }
+
+  selectTonality(tonality: TonalityValue) {
+    this.selectedTonality = tonality;
+    this.drawShape();
+  }
+
+  drawShape() {
+    this.shape = this.getShape();
+  }
+
+  abstract getShape(): Shape;
+
+  abstract initTone(): ToneValue;
+
+  abstract initShapeNo(): number;
+
+  abstract initTonality(): TonalityValue;
+}
