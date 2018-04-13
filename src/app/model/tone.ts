94fba3359a38d1caf6ffe3e601bd5d462d93d637
Michal Vala <michal.vala@gmail.com>
2018-04-13 07:16:30 +0200

    formatting

--- src/app/model/tone.ts
+++ src/app/model/tone.ts
@@ -14,19 +14,19 @@ export enum Tone {
 }
 
 export const ToneUp = {
-    'C': { key: 'C', name: 'C' },
-    'Csharp': { key: 'Csharp', name: 'C#' },
-    'D': { key: 'D', name: 'D' },
-    'Dsharp': { key: 'Dsharp', name: 'D#' },
-    'E': { key: 'E', name: 'E' },
-    'F': { key: 'F', name: 'F' },
-    'Fsharp': { key: 'Fsharp', name: 'F#' },
-    'G': { key: 'G', name: 'G' },
-    'Gsharp': { key: 'Gsharp', name: 'G#' },
-    'A': { key: 'A', name: 'A' },
-    'Asharp': { key: 'Asharp', name: 'A#' },
-    'B': { key: 'B', name: 'B' }
-  };
+  'C': { key: 'C', name: 'C' },
+  'Csharp': { key: 'Csharp', name: 'C#' },
+  'D': { key: 'D', name: 'D' },
+  'Dsharp': { key: 'Dsharp', name: 'D#' },
+  'E': { key: 'E', name: 'E' },
+  'F': { key: 'F', name: 'F' },
+  'Fsharp': { key: 'Fsharp', name: 'F#' },
+  'G': { key: 'G', name: 'G' },
+  'Gsharp': { key: 'Gsharp', name: 'G#' },
+  'A': { key: 'A', name: 'A' },
+  'Asharp': { key: 'Asharp', name: 'A#' },
+  'B': { key: 'B', name: 'B' }
+};
 
 export const ToneUpNames = Object.values(ToneUp).map(t => t.name);
 export const ToneUpKeys = Object.keys(ToneUp);
