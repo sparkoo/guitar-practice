268ede34b382bb24ce96eff780b66630466fbd3f
Michal Vala <michal.vala@gmail.com>
2018-04-13 21:57:40 +0200

    chords component

--- src/app/chords/chords.component.ts
+++ src/app/chords/chords.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Tonality, TonalityValue, ToneUp, ToneValue } from '../model/tone';
+import { Shape } from '../model/shape';
+import { ChordsService } from '../db/chords.service';
+import { ShapeBaseComponent } from '../shape-base.component';
 
 @Component({
   selector: 'app-chords',
   templateUrl: './chords.component.html',
   styleUrls: ['./chords.component.scss']
 })
-export class ChordsComponent implements OnInit {
+export class ChordsComponent extends ShapeBaseComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private chordShapesService: ChordsService) {
+    super();
   }
 
+  getShape(): Shape {
+    return this.chordShapesService.get(`${this.selectedTone.key}_${this.selectedTonality['name']}_${this.selectedShapeNo}`);
+  }
+
+  initShapeNo(): number {
+    return 1;
+  }
+
+  initTonality(): TonalityValue {
+    return Tonality.MAJOR;
+  }
+
+  initTone(): ToneValue {
+    return ToneUp.C;
+  }
 }
