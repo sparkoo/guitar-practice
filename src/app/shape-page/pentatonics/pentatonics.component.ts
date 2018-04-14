e8f464365d92d20825234b16ba9fc09aa187cfd2
Michal Vala <michal.vala@gmail.com>
2018-04-14 06:16:25 +0200

    pentatonics page

--- src/app/shape-page/pentatonics/pentatonics.component.ts
+++ src/app/shape-page/pentatonics/pentatonics.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Tonality, TonalityValue, ToneUp, ToneValue } from '../../model/tone';
+import { ChordsService } from '../../db/chords.service';
+import { Shape } from '../../model/shape';
+import { ShapeBaseComponent } from '../shape-base.component';
+import { PentatonicService } from '../../db/pentatonic.service';
 
 @Component({
   selector: 'app-pentatonics',
   templateUrl: './pentatonics.component.html',
-  styleUrls: ['./pentatonics.component.scss']
+  styleUrls: ['../shape-base.component.scss', './pentatonics.component.scss']
 })
-export class PentatonicsComponent implements OnInit {
+export class PentatonicsComponent extends ShapeBaseComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private pentatonicService: PentatonicService) {
+    super();
   }
 
+  getShape(): Shape {
+    return this.pentatonicService.get(`${this.selectedTone.key}_${this.selectedTonality['name']}_${this.selectedShapeNo}`);
+  }
+
+  initShapeNo(): number {
+    return 1;
+  }
+
+  initTonality(): TonalityValue {
+    return Tonality.MAJOR;
+  }
+
+  initTone(): ToneValue {
+    return ToneUp.C;
+  }
 }
