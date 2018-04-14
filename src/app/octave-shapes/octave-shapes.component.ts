c7075fb76af3b8e3faf498abf1d541f87b9cb890
Michal Vala <michal.vala@gmail.com>
2018-04-14 06:07:27 +0200

    octave shapes stylesheet from base

--- src/app/octave-shapes/octave-shapes.component.ts
+++ src/app/octave-shapes/octave-shapes.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { OctaveShapesService } from '../db/octave-shapes.service';
 import { ShapeBaseComponent } from '../shape-base.component';
 import { TonalityValue, ToneUp, ToneValue } from '../model/tone';
@@ -6,7 +6,7 @@ import { TonalityValue, ToneUp, ToneValue } from '../model/tone';
 @Component({
   selector: 'app-octave-shapes',
   templateUrl: './octave-shapes.component.html',
-  styleUrls: ['./octave-shapes.component.scss']
+  styleUrls: ['../shape-base.component.scss', './octave-shapes.component.scss']
 })
 export class OctaveShapesComponent extends ShapeBaseComponent {
   constructor(private octaveShapesService: OctaveShapesService) {
