dfe3d63a88cfd85ac5bf7341f88e846cb29f814c
Michal Vala <michal.vala@gmail.com>
2018-04-14 07:20:25 +0200

    #4 pass all db services to practice component

--- src/app/practice/practice.component.ts
+++ src/app/practice/practice.component.ts
@@ -3,6 +3,9 @@ import { ShapeBaseComponent } from '../shape-page/shape-base.component';
 import { ArpeggiosService } from '../db/arpeggios.service';
 import { SelectionService } from '../shape-page/selection.service';
 import { DbServiceBase } from '../db/db-service-base';
+import { ChordsService } from '../db/chords.service';
+import { OctaveShapesService } from '../db/octave-shapes.service';
+import { PentatonicService } from '../db/pentatonic.service';
 
 @Component({
   selector: 'app-practice',
@@ -12,7 +15,10 @@ import { DbServiceBase } from '../db/db-service-base';
 export class PracticeComponent extends ShapeBaseComponent {
 
   constructor(private selectionService: SelectionService,
-              private arpeggiosService: ArpeggiosService) {
+              private chordsService: ChordsService,
+              private arpeggiosService: ArpeggiosService,
+              private octaveShapesService: OctaveShapesService,
+              private pentatonicsService: PentatonicService) {
     super(selectionService);
   }
 
