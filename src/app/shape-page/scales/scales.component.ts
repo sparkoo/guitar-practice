890d7c84be0969160287bbcc93bfe4855b00783e
Michal Vala <michal.vala@gmail.com>
2018-04-15 14:02:00 +0200

    scale component to some real shape

--- src/app/shape-page/scales/scales.component.ts
+++ src/app/shape-page/scales/scales.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SelectionService } from '../selection.service';
+import { ShapeBaseComponent } from '../shape-base.component';
+import { DbServiceBase } from '../../db/db-service-base';
+import { ScalesService } from './scales.service';
 
 @Component({
   selector: 'app-scales',
   templateUrl: './scales.component.html',
-  styleUrls: ['./scales.component.scss']
+  styleUrls: ['../shape-base.component.scss', './scales.component.scss']
 })
-export class ScalesComponent implements OnInit {
+export class ScalesComponent extends ShapeBaseComponent {
 
-  constructor() { }
+  constructor(private selectionService: SelectionService,
+              private scalesService: ScalesService) {
+    super(selectionService);
+  }
 
-  ngOnInit() {
+  getDbService(): DbServiceBase {
+    return this.scalesService;
   }
 
 }
