7e76200697b75bdc232c8bb1457d1e81b3ff3283
Michal Vala <michal.vala@gmail.com>
2018-04-15 14:28:58 +0200

    minor scales

--- src/app/shape-page/scales/scales.service.ts
+++ src/app/shape-page/scales/scales.service.ts
@@ -47,11 +47,38 @@ export class ScalesService extends DbServiceBase {
 
   private static initMinorFingers(): any[] {
     const fingers = [];
-    fingers[0] = { 0: ['g'], 1: ['E', 'a', 'd', 'b', 'e'], 3: ['E', 'a', 'd', 'g', 'e'], 4: ['b'] };
-    fingers[1] = { 1: ['E', 'a', 'd', 'g', 'e'], 2: ['b'], 3: ['d', 'g'], 4: ['E', 'a', 'b', 'e'] };
-    fingers[2] = { 1: ['d', 'g'], 2: ['E', 'a', 'b', 'e'], 4: ['E', 'a', 'd', 'g', 'b', 'e'] };
-    fingers[3] = { 1: ['E', 'a', 'd', 'g', 'b', 'e'], 3: ['a', 'd', 'g'], 4: ['E', 'b', 'e'] };
-    fingers[4] = { 1: ['a', 'd', 'g'], 2: ['E', 'b', 'e'], 3: ['g'], 4: ['E', 'a', 'd', 'b', 'e'] };
+    fingers[0] = {
+      0: ['d', 'g'],
+      1: ['E', 'a', 'd', 'g', 'b', 'e'],
+      3: ['E', 'a', 'd', 'g', 'b', 'e'],
+      4: ['E', 'b', 'e']
+    };
+    fingers[1] = {
+      1: ['E', 'a', 'd', 'g', 'b', 'e'],
+      2: ['E', 'b', 'e'],
+      3: ['a', 'd', 'g'],
+      4: ['E', 'a', 'd', 'b', 'e']
+    };
+    fingers[2] = {
+      1: ['a', 'd', 'g'],
+      2: ['E', 'a', 'd', 'b', 'e'],
+      3: ['g'],
+      4: ['E', 'a', 'd', 'g', 'b', 'e'],
+      5: ['b']
+    };
+    fingers[3] = {
+      0: ['g'],
+      1: ['E', 'a', 'd', 'g', 'b', 'e'],
+      2: ['b'],
+      3: ['E', 'a', 'd', 'g', 'e'],
+      4: ['E', 'a', 'b', 'e']
+    };
+    fingers[4] = {
+      1: ['E', 'a', 'd', 'g', 'e'],
+      2: ['E', 'a', 'b', 'e'],
+      3: ['d', 'g'],
+      4: ['E', 'a', 'd', 'g', 'b', 'e']
+    };
     return fingers;
   }
 
